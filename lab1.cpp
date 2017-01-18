1ea92fc3a8e629798ba4b090edefc7f20d5abee3
Shane McSally <shane.mcsally@gmail.com>
2017-01-18 14:56:33 -0500

    Adds 2 new files. Code file(.cpp), compiled executable file (a.out)

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double elements;
+	double sum = 0;
+	double temp;
+	cout << "Enter a number of elements: ";
+	cin >> elements;
+	for(int i = 0; i < elements; i++)
+	{
+		cout << "Enter value: ";
+		cin >> temp;
+		sum += temp;
+	}
+	cout << "Average is " << sum/elements << endl;
+
